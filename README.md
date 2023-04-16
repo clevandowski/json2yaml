5da2b5eb4e8800a2c746c15538fa87afaccfc5c7
clevandowski <cyrille.levandowski@gmail.com>
2023-04-16 18:28:10 +0200

    1st commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# yaml2json
+
+## Build
+
+Requires go >= v1.20
+
+```bash
+go build
+```
+
+## Install
+
+```bash
+sudo ln -s ${PWD}/json2yaml /usr/local/bin/json2yaml
+```
+
+## Usage
+
+```bash
+cat myfile.json | json2yaml
+```
+
+```bash
+echo '{ "test": 3 }' | json2yaml
+```
---
+++ json2yaml.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+  "fmt"
+  "gopkg.in/yaml.v3"
+  "io"
+  "log"
+  "os"
+  "strings"
+)
+
+func json2yaml(data string) (string, error) {
+  data = strings.TrimSpace(data)
+  if data == "" {
+    return "", nil
+  }
+  var m any
+  err := json.Unmarshal([]byte(data), &m)
+  if err != nil {
+    return "", err
+  }
+
+  output, err := yaml.Marshal(&m)
+  if err != nil {
+    return "", err
+  }
+  return fmt.Sprintf("%v", string(output)), nil
+}
+
+func main() {
+  stdin, err := io.ReadAll(os.Stdin)
+
+	if err != nil {
+		panic(err)
+	}
+	data := string(stdin)
+
+  output, err := json2yaml(data)
+  if err != nil {
+    log.Fatalf("error: %v", err)
+  }
+  fmt.Printf("%v", string(output))
+}
